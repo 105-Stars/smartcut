d2ec3775ab8f1c5410d5a596a701c3d0f20ed752
105-Stars <1052366296@qq.com>
2026-07-11 09:42:04 +0800

    Optimize robots.txt and add sitemap.xml for Google AdSense approval

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://starcloud.dpdns.org', // 你的域名
+      dynamicRoutes: [
+        '/', 
+        '/blog', 
+        // 如果你有具体的文章路径，可以手动加在这里，或者让插件自动扫描
+      ],
+      generateRobotsTxt: false, 
+    })
+  ],
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
